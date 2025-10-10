20493312f668191135d15a64085f9c203213b5e8
Nourhan021 <nourhanahmeed690@gmail.com>
2025-10-10 18:02:11 +0300

    Write here what you changed

--- lib/services/firebase_auth_service.dart
+++ lib/services/firebase_auth_service.dart
@@ -1,6 +1,54 @@
-// login
 // register
+// login
 // logout
 // static getter => current user
 // update password
 // update email
+
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthenticationService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  //register
+  Future<User?> register({
+    required String email,
+    required String password,
+  }) async {
+    try {
+      UserCredential result = await _auth.createUserWithEmailAndPassword(
+        email: email.trim(),
+        password: password.trim(),
+      );
+      return result.user;
+    } on FirebaseAuthException catch (e) {
+      print("Register Error: ${e.message}");
+      rethrow;
+    }
+  }
+
+  //login
+  Future<User?> login({required String email, required String password}) async {
+    try {
+      UserCredential result = await _auth.signInWithEmailAndPassword(
+        email: email.trim(),
+        password: password.trim(),
+      );
+      return result.user; 
+    } on FirebaseAuthException catch (e) {
+      print("Login Error: ${e.message}");
+      rethrow; 
+    }
+  }
+
+  //logout
+  Future<void> logout() async {
+    try {
+      await _auth.signOut();
+      print("Logged Out Successfully");
+    } catch (e) {
+      print("Logout Error: $e");
+      rethrow;
+    }
+  }
+}
