bc621dc0d8c4514c82167b40be701bc2b8b2de69
youssefjoe18 <youssefzoghbi18@gmail.com>
2025-10-17 18:43:38 +0300

    Firebase Auth

--- lib/services/firebase_auth_service.dart
+++ lib/services/firebase_auth_service.dart
@@ -4,6 +4,7 @@
 // static getter => current user
 // update password
 // update email
+//delete user
 
 import 'package:firebase_auth/firebase_auth.dart';
 
@@ -48,6 +49,64 @@ class AuthenticationService {
       print("Logged Out Successfully");
     } catch (e) {
       print("Logout Error: $e");
+      rethrow;
+    }
+  }
+  // --- Update Password ---
+  Future<void> updatePassword(String newPassword) async {
+    try {
+      User? user = _auth.currentUser;
+      if (user != null) {
+        await user.updatePassword(newPassword.trim());
+        await user.reload(); // refresh user info
+        print("Password updated successfully");
+      } else {
+        throw FirebaseAuthException(
+          code: "user-not-found",
+          message: "No user currently signed in.",
+        );
+      }
+    } on FirebaseAuthException catch (e) {
+      print("Update Password Error: ${e.message}");
+      rethrow;
+    }
+  }
+
+  // --- Update Email ---
+ Future<void> updateEmail(String newEmail) async {
+  try {
+    final user = FirebaseAuth.instance.currentUser;
+    if (user != null) {
+      await user.verifyBeforeUpdateEmail(newEmail.trim());
+      print("Verification email sent to new email address.");
+    } else {
+      throw FirebaseAuthException(
+        code: "user-not-found",
+        message: "No user currently signed in.",
+      );
+    }
+  } on FirebaseAuthException catch (e) {
+    print("Update Email Error: ${e.message}");
+    rethrow;
+  }
+}
+
+
+  // --- Delete User ---
+  Future<void> deleteUser() async {
+    try {
+      User? user = _auth.currentUser;
+      if (user != null) {
+        await user.delete();
+        print("User deleted successfully");
+      } else {
+        throw FirebaseAuthException(
+          code: "user-not-found",
+          message: "No user currently signed in.",
+        );
+      }
+    } on FirebaseAuthException catch (e) {
+      print("Delete User Error: ${e.message}");
       rethrow;
     }
   }
