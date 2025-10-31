b1b55428f5c5ce58f91c6db447a02b7e31aff157
OmarAhmedAli-OA <oa235711@gmail.com>
2025-10-31 20:37:30 +0200

    Initial commit

--- lib/services/firestore.dart
+++ lib/services/firestore.dart
@@ -1,27 +1,16 @@
-// collections investor, entrepreneur, company, requests.
-// investor ==> name*, profilePhotoUrl*, about*, phoneNumber*, Experience*, skills*, investmentCapacity*, NationalIdUrl*, PreferredIndustries*, InvestorType, uid
-// entrepreneur ==> profilePhotoUrl* name*, about*, phoneNumber*, experience*, skills*, role*, NationalIdUrl*, uid
-// company ==> name*, description*, founded*, teamSize*, industry*, stage*, currency*, location*, teamMembers* list<map<>> => name, role*, verifiedCertificate*, uid, createdAt
-// request ==> description*, amountOfMoney*, equityInReturn*, whyAreYouRaising*, submittedAt, companyid, uid
-
-// CRUD
-// addCompany, addRequest, addInvestor, addEntrepreneur,
-// read data,
-// update data,
-// delete request,
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:depi_graduation_project/models/investor.dart';
 import 'package:depi_graduation_project/models/request.dart';
 import 'package:depi_graduation_project/models/company.dart';
 import 'package:depi_graduation_project/models/entrepreneur.dart';
+import 'package:depi_graduation_project/models/chat_room.dart';
+import 'package:depi_graduation_project/models/message.dart';
 
-// global variable _db to access firestore functions to use in all 4 classes
+// Global variable _db to access firestore functions to use in all 4 classes
 final FirebaseFirestore _db = FirebaseFirestore.instance;
 
-// functions addInvestor, updateInvestor, getInvestorStream, getInvestors, deleteInvestor
 class InvestorFirestoreService {
-  // Function to add investor
+  // Add investor
   Future<void> addInvestor({required String name, required String uid}) async {
     await _db.collection('investors').doc(uid).set({
       'name': name,
@@ -37,7 +26,7 @@ class InvestorFirestoreService {
     });
   }
 
-  // Function to update investor data all at once requires map<String, dynamic>
+  // Update investor data all at once requires map<String, dynamic>
   Future<void> updateInvestor({
     required String uid,
     required Map<String, dynamic> updatedData,
@@ -45,7 +34,7 @@ class InvestorFirestoreService {
     await _db.collection('investors').doc(uid).update(updatedData);
   }
 
-  // Function to get stream of investor object => any thing changes in the firestore the investor changes as well, mainly for his profile page when he updates data.
+  // Get stream of investor object => any thing changes in the firestore the investor changes as well, mainly for his profile page when he updates data.
   Stream<Investor> getInvestorStream({required String uid}) {
     return _db
         .collection('investors')
@@ -54,13 +43,13 @@ class InvestorFirestoreService {
         .map((snapshot) => Investor.fromFireStore(snapshot.data() ?? {}, uid));
   }
 
-  // get investor once
+  // Get investor once
   Future<Investor> getInvestor({required String uid}) async {
     final doc = await _db.collection('investors').doc(uid).get();
     return Investor.fromFireStore(doc.data() ?? {}, uid);
   }
 
-  // Function to get all investors as objects, for entrepreneur to see all investors
+  // Get all investors as objects, for entrepreneur to see all investors
   Future<List<Investor>> getInvestors() async {
     final snapshot = await _db.collection('investors').get();
     return snapshot.docs.map((doc) {
@@ -68,7 +57,7 @@ class InvestorFirestoreService {
     }).toList();
   }
 
-  // update profile photo url
+  // Update profile photo url
   Future<void> updateInvestorProfilePhotoUrl({
     required String uid,
     required String newUrl,
@@ -76,7 +65,7 @@ class InvestorFirestoreService {
     await _db.collection('investors').doc(uid).update({'photoUrl': newUrl});
   }
 
-  // update id photo url
+  // Update id photo url
   Future<void> updateInvestorNationalIdUrl({
     required String uid,
     required String newUrl,
@@ -86,14 +75,14 @@ class InvestorFirestoreService {
     });
   }
 
-  // Function to delete investor document in investors collection, used if investor deletes his account
+  // Delete investor document in investors collection, used if investor deletes his account
   Future<void> deleteInvestor({required String uid}) async {
     await _db.collection('investors').doc(uid).delete();
   }
 }
 
 class RequestFirestoreService {
-  //  Add request
+  // Add request
   Future<void> addRequest({
     required String uid,
     required String description,
@@ -111,7 +100,7 @@ class RequestFirestoreService {
     });
   }
 
-  //  Update request by document ID
+  // Update request by document ID
   Future<void> updateRequest({
     required String requestId,
     required Map<String, dynamic> updatedData,
@@ -119,7 +108,7 @@ class RequestFirestoreService {
     await _db.collection('requests').doc(requestId).update(updatedData);
   }
 
-  //  Stream of user's requests
+  // Stream of user's requests
   Stream<List<Request>> getRequestsStream({required String uid}) {
     return _db
         .collection('requests')
@@ -133,7 +122,7 @@ class RequestFirestoreService {
         );
   }
 
-  //  Get all requests
+  // Get all requests
   Future<List<Request>> getRequests() async {
     final snapshot = await _db.collection('requests').get();
     return snapshot.docs
@@ -141,7 +130,7 @@ class RequestFirestoreService {
         .toList();
   }
 
-  // return stream of one request
+  // Return stream of one request
   Stream<Request?> getRequest(String uid, String requestId) {
     return _db.collection('requests').doc(requestId).snapshots().map((
       docSnapshot,
@@ -161,7 +150,7 @@ class RequestFirestoreService {
 }
 
 class CompanyFirestoreService {
-  // Function to add company
+  // Add company
   Future<void> addCompany({required String name, required String uid}) async {
     await _db.collection('companies').doc(uid).set({
       'name': name,
@@ -179,7 +168,7 @@ class CompanyFirestoreService {
     });
   }
 
-  // Function to update company data all at once requires map<String, dynamic>
+  // Update company data all at once requires map<String, dynamic>
   Future<bool> updateCompany({
     required String uid,
     required Map<String, dynamic> updatedData,
@@ -215,7 +204,7 @@ class CompanyFirestoreService {
     return Company.fromFireStore(doc.data() ?? {}, uid);
   }
 
-  // Function to get all companies as objects, for entrepreneur to see all companies
+  // Get all companies as objects, for entrepreneur to see all companies
   Future<List<Company>> getCompanies() async {
     final snapshot = await _db.collection('companies').get();
     return snapshot.docs.map((doc) {
@@ -223,7 +212,7 @@ class CompanyFirestoreService {
     }).toList();
   }
 
-  // update company logo url
+  // Update company logo url
   Future<void> updateCompanyLogoUrl({
     required String uid,
     required String newUrl,
@@ -231,7 +220,7 @@ class CompanyFirestoreService {
     await _db.collection('companies').doc(uid).update({'logoUrl': newUrl});
   }
 
-  // update company certificate url
+  // Update company certificate url
   Future<void> updateCompanyCertificateUrl({
     required String uid,
     required String newUrl,
@@ -241,7 +230,7 @@ class CompanyFirestoreService {
     });
   }
 
-  // Function to delete company document in companies collection, used if company deletes his account
+  // Delete company document in companies collection, used if company deletes his account
   Future<void> deleteCompany({required String uid}) async {
     await _db.collection('companies').doc(uid).delete();
   }
@@ -284,7 +273,7 @@ class EntrepreneurFirestoreService {
         );
   }
 
-  // get entrepreneur once
+  // Get entrepreneur once
   Future<Entrepreneur> getEntrepreneur({required String uid}) async {
     final doc = await _db.collection('entrepreneurs').doc(uid).get();
     return Entrepreneur.fromFireStore(doc.data() ?? {}, uid);
@@ -298,7 +287,7 @@ class EntrepreneurFirestoreService {
     }).toList();
   }
 
-  // update profile photo url
+  // Update profile photo url
   Future<void> updateEntrepreneurProfilePhotoUrl({
     required String uid,
     required String newUrl,
@@ -308,7 +297,7 @@ class EntrepreneurFirestoreService {
     });
   }
 
-  // update id photo url
+  // Update id photo url
   Future<void> updateEntrepreneurNationalIdUrl({
     required String uid,
     required String newUrl,
@@ -321,5 +310,168 @@ class EntrepreneurFirestoreService {
   // Delete entrepreneur
   Future<void> deleteEntrepreneur({required String uid}) async {
     await _db.collection('entrepreneurs').doc(uid).delete();
+  }
+}
+class ChatRoomFirestoreService {
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  // Chat Room Management
+
+  Future<void> addOrGetChatRoom(String uid1, String uid2) async {
+    final chatRooms = _firestore.collection('chatrooms');
+
+    final chatRoomId = uid1.compareTo(uid2) <= 0 ? '${uid1}_$uid2' : '${uid2}_$uid1';
+    final chatRoomDoc = chatRooms.doc(chatRoomId);
+    final chatRoomSnap = await chatRoomDoc.get();
+    if (!chatRoomSnap.exists) {
+      await chatRoomDoc.set({
+        'id': chatRoomId,
+        'members': [uid1, uid2],
+        'timeCreated': FieldValue.serverTimestamp(),
+        'lastMessage': '',
+        'lastMessageTime': FieldValue.serverTimestamp(),
+      });
+    }
+  }
+
+  Future<void> deleteChatRoom(String chatRoomId) async {
+    final chatRoomRef = _firestore.collection('chatrooms').doc(chatRoomId);
+
+    final messages = await chatRoomRef.collection('messages').get();
+    for (var doc in messages.docs) {
+      await doc.reference.delete();
+    }
+
+    await chatRoomRef.delete();
+  }
+
+  Stream<List<ChatRoom>> getChatRoomStream(String uid) {
+    return _firestore
+        .collection('chatrooms')
+        .where('members', arrayContains: uid)
+        .orderBy('lastMessageTime', descending: true)
+        .snapshots()
+        .map((snapshot) => snapshot.docs
+            .map((doc) => ChatRoom.fromMap(doc.data()))
+            .toList());
+  }
+
+  // Message Management
+
+  Future<void> addMessage(
+    String chatRoomId,
+    String content,
+    String senderId,
+    String receiverId,
+  ) async {
+    final messageRef = _firestore
+        .collection('chatrooms')
+        .doc(chatRoomId)
+        .collection('messages')
+        .doc();
+
+    final messageData = {
+      'id': messageRef.id,
+      'content': content,
+      'senderId': senderId,
+      'receiverId': receiverId,
+      'timeCreated': FieldValue.serverTimestamp(),
+      'isRead': false,
+      'isEdited': false,
+    };
+
+    await messageRef.set(messageData);
+    await updateLastMessage(chatRoomId, content);
+  }
+
+  Future<void> deleteMessage(String chatRoomId, String messageId) async {
+    final messageRef = _firestore
+        .collection('chatrooms')
+        .doc(chatRoomId)
+        .collection('messages')
+        .doc(messageId);
+
+    await messageRef.delete();
+  }
+
+  Future<void> updateMessage(
+      String chatRoomId, String messageId, String newContent) async {
+    final messageRef = _firestore
+        .collection('chatrooms')
+        .doc(chatRoomId)
+        .collection('messages')
+        .doc(messageId);
+
+    await messageRef.update({
+      'content': newContent,
+      'isEdited': true,
+    });
+  }
+
+  Stream<List<Message>> getMessagesStream(String chatRoomId) {
+    return _firestore
+        .collection('chatrooms')
+        .doc(chatRoomId)
+        .collection('messages')
+        .orderBy('timeCreated', descending: false)
+        .snapshots()
+        .map((snapshot) =>
+            snapshot.docs.map((doc) => Message.fromMap(doc.data())).toList());
+  }
+
+  // Message States
+
+  Future<void> toggleIsRead(
+      String chatRoomId, String messageId, bool value) async {
+    await _firestore
+        .collection('chatrooms')
+        .doc(chatRoomId)
+        .collection('messages')
+        .doc(messageId)
+        .update({'isRead': value});
+  }
+
+  Future<void> toggleIsEdited(
+      String chatRoomId, String messageId, bool value) async {
+    await _firestore
+        .collection('chatrooms')
+        .doc(chatRoomId)
+        .collection('messages')
+        .doc(messageId)
+        .update({'isEdited': value});
+  }
+
+  Future<void> markAllAsRead(String chatRoomId, String currentUserId) async {
+    final unreadMessages = await _firestore
+        .collection('chatrooms')
+        .doc(chatRoomId)
+        .collection('messages')
+        .where('receiverId', isEqualTo: currentUserId)
+        .where('isRead', isEqualTo: false)
+        .get();
+
+    for (var doc in unreadMessages.docs) {
+      await doc.reference.update({'isRead': true});
+    }
+  }
+
+  // Chat room updates 
+
+  Future<void> updateLastMessage(String chatRoomId, String content) async {
+    await _firestore.collection('chatrooms').doc(chatRoomId).update({
+      'lastMessage': content,
+      'lastMessageTime': FieldValue.serverTimestamp(),
+    });
+  }
+
+  // Unread messages tracking
+
+  Stream<int> getUnreadCountStream(String uid) {
+    return _firestore
+        .collectionGroup('messages')
+        .where('receiverId', isEqualTo: uid)
+        .where('isRead', isEqualTo: false)
+        .snapshots()
+        .map((snapshot) => snapshot.docs.length);
   }
 }
